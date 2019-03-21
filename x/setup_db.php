c0994235f95bc17604e42c7146d56460acf99f8a
geronimodennis <geronimodennis@gmail.com>
2019-03-21 15:19:53 +0800

    Add files via upload

--- x/setup_db.php
+++ x/setup_db.php
@@ -6,20 +6,34 @@
  * Time: 1:02 AM
  */
 
-$databaseName = 'xyz    _db';
+$databaseName = 'project_e_db';
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+echo "<b>Database Installation</b>";
 
-$conn = new mysqli('localhost', 'root');
+$conn = new mysqli($hostname, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "CREATE DATABASE $databaseName";
 if ($conn->query($sql) === TRUE) {
+    echo "<br>";
     echo "Database created successfully";
 } else {
+    echo "<br>";
     echo "Error creating database: " . $conn->error;
 }
+$conn->close();
 
-
-
+$conn = new mysqli($hostname, $username, $password, $databaseName);
+$sql = "CREATE TABLE loginCredentials ( `id` BIGINT NOT NULL AUTO_INCREMENT , `username` VARCHAR(50) NOT NULL , `password` VARCHAR(50) NOT NULL , `userId` BIGINT NOT NULL , PRIMARY KEY (`id`))";
+if ($conn->query($sql) === TRUE) {
+    echo "<br>";
+    echo "Database table created successfully";
+} else {
+    echo "<br>";
+    echo "Error creating database table: " . $conn->error;
+}
 $conn->close();
