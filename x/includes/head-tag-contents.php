0545f99b7ec8c6f396b56d86fd4c02b2eaa7b12a
geronimodennis <geronimodennis@gmail.com>
2019-03-22 17:31:46 +0800

    Add files via upload

---
+++ x/includes/head-tag-contents.php
@@ -0,0 +1,14 @@
+<title>Academic Education V2</title>
+
+<script type="text/javascript" src="vendor/jquery/dist/jquery.slim.min.js" ></script>
+<script src="vendor/popper.min.js" ></script>
+<script src="vendor/bootstrap/dist/js/bootstrap.js" ></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $(".dropdown-toggle").dropdown();
+    });
+</script>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="vendor/bootstrap/dist/css/bootstrap.min.css">
+
---
+++ x/setup_db.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dengero
+ * Date: 21/03/2019
+ * Time: 1:02 AM
+ */
+
+$databaseName = 'project_e_db';
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+echo "<b>Database Installation</b>";
+
+$conn = new mysqli($hostname, $username, $password);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "CREATE DATABASE $databaseName";
+if ($conn->query($sql) === TRUE) {
+    echo "<br>Database created successfully";
+} else {
+    echo "<br>Error creating database: " . $conn->error;
+}
+
+$conn->select_db($databaseName);
+$fl = file_get_contents('setup_db.sql');
+if($conn->multi_query($fl)== true){
+    echo "<br>SQL execution has been successful";
+}else{
+    echo "<br>" . $conn->error;
+}
